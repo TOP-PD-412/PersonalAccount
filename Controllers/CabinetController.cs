a446225ac5f6215b38d1b233b7d085d1df3c3efe
Evgeniy <eugene020292@gmail.com>
2026-05-23 20:41:45 +0300

    View->Cabinet->Edit/Index

--- Controllers/CabinetController.cs
+++ Controllers/CabinetController.cs
@@ -25,4 +25,54 @@ public class CabinetController(IStudentAuthService students) : Controller
         }
         return View(student);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit()
+    {
+        var rawId = User.GetId();
+        if (rawId == null) return RedirectToAction("Error", "Home");
+
+        int studentId = Convert.ToInt32(rawId.Value);
+
+        var student = await students.GetByIdAsync(studentId);
+
+        if (student == null) return RedirectToAction("Error", "Home");
+
+        var editModel = new StudentEditModel
+        {
+            FullName = student.FullName,
+            GroupName = student.GroupName,
+            PhotoUrl = student.PhotoUrl
+        };
+
+        return View(editModel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken] 
+    public async Task<IActionResult> Edit(StudentEditModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model); 
+        }
+
+        var rawId = User.GetId();
+        if (rawId == null) return RedirectToAction("Error", "Home");
+
+        int studentId = Convert.ToInt32(rawId.Value);
+
+        var updatedStudent = new StudentModel
+        {
+            FullName = model.FullName,
+            GroupName = model.GroupName,
+            PhotoUrl = model.PhotoUrl
+        };
+
+        await students.UpdateByIdAsync(studentId, updatedStudent);
+
+        return RedirectToAction("Index");
+    }
 }
+
+
